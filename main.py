620b8681efc34c79587ba2f5f058fbc18f6ab2d4
Papp Adam <adam.papp123@gmail.com>
2024-05-03 12:53:00 +0200

    WIP

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 import threading
 
-def get_files(ignore_list):
-    source_directory = os.path.join(os.getcwd(), 'source')
+def get_files(source_directory, ignore_list):
     
     print("Reading files from: " + source_directory)
     file_list = []
@@ -33,50 +32,61 @@ def get_rules():
         rules = file.readlines()
     return rules
 
-def process_file(file):
+def process_file(file, result_directory, rules):
     # Add your code here to modify the file
     print("Processing file:", file)
+    result_list = []
+    for line in file:
+        for rule in rules:
+            if rule in line:
+                # Modify the line, so the rule is not present
+                line = line.replace(rule, "")
+        result_list.append(line)
+    
+    # Write the modified content to a new file in the results directory
+    result_file = os.path.join(result_directory, os.path.basename(file))
+    with open(result_file, 'w') as file:
+        file.writelines(result_list)
+        
+    print("File processed:", file)
 
-def process_files(files):
+def process_files(files, result_directory, rules):
     threads = []
     for file in files:
-        thread = threading.Thread(target=process_file, args=(file,))
+        thread = threading.Thread(target=process_file, args=(file,result_directory,rules,))
         thread.start()
         threads.append(thread)
 
     # Wait for all threads to finish
     for thread in threads:
         thread.join()
-        
-    ## TODO: Check file names and modify them as per the rules
+  
+    # Call the function to check and modify file names
+    check_file_names(result_directory, rules)
 
+def check_file_names(result_directory, rules):
+    for file_name in os.listdir(result_directory):
+        for rule in rules:
+            if rule in file_name:
+                # Modify the file name, so the rule is not present
+                new_file_name = file_name.replace(rule, "")
+                old_file_path = os.path.join(result_directory, file_name)
+                new_file_path = os.path.join(result_directory, new_file_name)
+                os.rename(old_file_path, new_file_path)
+                print("File name modified:", file_name, "->", new_file_name)
 
 
 def __main__():
-    # Make results directory, include all subdirectories from source
-    # Make a list of all files in the source directory
-    # Read the ignore list
-    # Read the rules
-    # Process the files
-    # Write the results to the results directory
     
+    source_directory = os.path.join(os.getcwd(), 'source')
     results_directory = os.path.join(os.getcwd(), 'results')
     os.makedirs(results_directory, exist_ok=True)
-
-    source_directory = os.path.join(os.getcwd(), 'source')
-    for root, dirs, files in os.walk(source_directory):
-        for dir in dirs:
-            dir_path = os.path.join(root, dir)
-            relative_path = os.path.relpath(dir_path, source_directory)
-            results_subdirectory = os.path.join(results_directory, relative_path)
-            os.makedirs(results_subdirectory, exist_ok=True)
+    ignore_list = get_ignore_list()
+    rules = get_rules()
     
-    files = get_files(get_ignore_list())
-    for file in files:
-        #print(file)
-        continue
+    file_list = get_files(source_directory, ignore_list)
     
     # Call the function to process the files
-    process_files(files)
+    process_files(file_list, results_directory, rules)
         
 __main__()
