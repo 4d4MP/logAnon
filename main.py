2cd1721cd7961b6ea052010d821f765a788c5966
Papp Adam <adam.papp123@gmail.com>
2024-05-13 13:03:07 +0200

    Bug fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import threading
+import re
 
 maintain_character_length = True
 
@@ -9,12 +10,17 @@ def get_files(source_directory, ignore_list):
 
     for root, dirs, files in os.walk(source_directory):
         for file in files:
-            if not any(file.endswith(ignore) for ignore in ignore_list):
-                file_path = os.path.join(root, file)
-                file_list.append(file_path)
-            else:
-                print("Ignoring file: " + file)
-
+            file_path = os.path.join(root, file)
+            file_list.append(file_path)
+            
+    # Remove files in ignore list
+    for ignore in ignore_list:
+        ignore = ignore.strip()
+        for file in file_list:
+            if file.endswith(ignore):
+                file_list.remove(file)
+                print("File ignored:", file)
+                
     return file_list
 
 def get_ignore_list():
@@ -27,33 +33,60 @@ def get_ignore_list():
     return ignore_list
 
 def get_rules():
+    print("Reading rules from: " + os.path.join(os.getcwd(), 'main.rule'))
     rules_file = os.path.join(os.getcwd(), 'main.rule')
     rules = []
     with open(rules_file, 'r') as file:
         rules = file.readlines()
-    return rules
+        
+    for rule in rules:
+        if rule.startswith("#"):
+            rules.remove(rule)
+            
+            
+    print("Rules to be applied:")
+    for rule in rules:
+        print("      - " + rule)
+        
+    # Return rules string as regex
+    regex_rules = []
+    for i in range(len(rules)):
+        rule = rules[i].strip()
+        regex_rule = re.escape(rule)  # Convert rule to regex expression
+        regex_rules.append(regex_rule)
+    
+    return regex_rules
 
 def process_file(file, result_directory, rules):
     result_list = []
-    for line in file:
+    if file.endswith('.xel'):
+        return None
+    
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        
+    for line in lines:
         for rule in rules:
-            if rule in line:
+            #print("Checking for rule:", rule, "in line:", line)
+            if re.search(rule, line):
                 # Modify the line, so the rule is not present
+                print("Rule found in file:", file.split('\\')[-1], "\n    Rule:", rule, "\n    Line:", line)
                 line = line.replace(rule, get_replace_str(rule))
+                print("Line modified:", line)
         result_list.append(line)
     
     # Write the modified content to a new file in the results directory
-    result_file = os.path.join(result_directory, os.path.basename(file))
-    with open(result_file, 'w') as file:
+    with open(os.path.join(result_directory, os.path.basename(file)), 'w') as file:
         file.writelines(result_list)
         
-    print("File processed:", file.name + "\n")
+    print("File processed:", file.name)
 
 def process_files(files, result_directory, rules):
     threads = []
     for file in files:
         if file != None:
             thread = threading.Thread(target=process_file, args=(file,result_directory,rules,))
+            thread.name = file + " Thread"
             thread.start()
             threads.append(thread)
 
